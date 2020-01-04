d6b9ae4fad5a371757aa44fd84c9457a84d004ff
HYmodrich <lwj0337@naver.com>
2020-01-04 16:36:53 +0900

    c2j150

--- shell/start_debug.sh
+++ shell/start_debug.sh
@@ -9,13 +9,14 @@ STORAGE='nvme0n1'
 
 echo 0 > /proc/sys/kernel/randomize_va_space
 cat /proc/sys/kernel/randomize_va_space
-
-#for i in 10
-# do
+for STORAGE in sdd1 sdc1 sdb1 
+do
+for i in 16 48
+ do
+ echo $STORAGE thread $i
 #  for j in 1 2 3 4 5
 #  do
 
-  i=4
   umount mnt
 
   echo y | mkfs.ext4 -E lazy_itable_init=0,lazy_journal_init=0 /dev/$STORAGE
@@ -34,8 +35,8 @@ cat /proc/sys/kernel/randomize_va_space
   free -h && sync && sh -c 'echo 3 > /proc/sys/vm/drop_caches' && free -h
 
   filebench -f workload/varmail$i.f >> output/${STORAGE}thread$i.txt 
-# done
+ done
 # echo output/${STORAGE}thread$i.txt 
 
-#done
+done
 
--- shell/start_sysbench.sh
+++ shell/start_sysbench.sh
@@ -6,47 +6,51 @@
 #echo 0 > /proc/sys/kernel/randomize_va_space
 #cat /proc/sys/kernel/randomize_va_space
 
-OUTDIR="$(pwd)/output/sysbench-ext4"
+OUTDIR="$(pwd)/output/sysbench"
+
 for STORAGE in nvme0n1 sdd1 sdc1 sdb1
 do
   echo ======== $STORAGE ========
   for i in 1 10 20 30 40 50
   do
-    CHECK=$(umount mnt 2>&1 | grep busy);
-    echo $CHECK
-    if [ "$CHECK" = "" ]; then 
-      echo -- UMOUNT COMPLETE 
-    else
-      echo !!!!UMOUNT FAIL!!!!
-      fuser -ck mnt
-      CHECK1=$(umount -l mnt 2>&1 | grep busy);
-      if [ "$CHECK1" = "" ]; then 
-        echo -- UMOUNT COMPLETE 
-      else
-        echo !!!!UMOUNT FAIL!!!!
-        exit 0
-      fi
-    fi
-    echo y | mkfs.ext4 -E lazy_itable_init=0,lazy_journal_init=0 /dev/$STORAGE;
+    for iter in 1 2 3
+	  do
+	    CHECK=$(umount mnt 2>&1 | grep busy);
+	    echo $CHECK
+	    if [ "$CHECK" = "" ]; then 
+	      echo -- UMOUNT COMPLETE 
+	    else
+	      echo !!!!UMOUNT FAIL!!!!
+	      fuser -ck mnt
+	      CHECK1=$(umount -l mnt 2>&1 | grep busy);
+	      if [ "$CHECK1" = "" ]; then 
+		echo -- UMOUNT COMPLETE 
+	      else
+		echo !!!!UMOUNT FAIL!!!!
+		exit 0
+	      fi
+	    fi
+	    echo y | mkfs.ext4 -E lazy_itable_init=0,lazy_journal_init=0 /dev/$STORAGE;
 
-  # data journaling mode
-  ## mount -o data=journal /dev/nvme0n1p1 /home/dertflag/mnt
+	  # data journaling mode
+	  ## mount -o data=journal /dev/nvme0n1p1 /home/dertflag/mnt
 
-    mount /dev/$STORAGE mnt;
-    cd mnt;
+	    mount /dev/$STORAGE mnt;
+	    cd mnt;
 
-  #free all data in memory
-    free -h && sync && sh -c 'echo 3 > /proc/sys/vm/drop_caches' && free -h;
+	  #free all data in memory
+	    free -h && sync && sh -c 'echo 3 > /proc/sys/vm/drop_caches' && free -h;
 
-    sysbench --test=fileio --file-total-size=20G prepare
-    sysbench --test=fileio --file-total-size=20G --file-test-mode=rndrw --file-fsync-all=on --num-threads=$i --max-time=60 --max-requests=0 run >> ${OUTDIR}/${STORAGE}thread$i;
-   # sysbench --test=fileio --file-total-size=20G cleanup
+	    sysbench --test=fileio --file-total-size=150G prepare
+	    sysbench --test=fileio --file-total-size=150G --file-test-mode=rndrw --file-fsync-all=on --num-threads=$i --max-time=60 --max-requests=0 run >> ${OUTDIR}/${STORAGE}thread$i;
+	   # sysbench --test=fileio --file-total-size=20G cleanup
 
-    cd ..;
-#filebench -f workload/varmail$i.f >> output/${STORAGE}thread$i.txt 
-# done
-# echo output/${STORAGE}thread$i.txt 
+	    cd ..;
+	#filebench -f workload/varmail$i.f >> output/${STORAGE}thread$i.txt 
+	# done
+	# echo output/${STORAGE}thread$i.txt 
 
+	done
   done
 done
 

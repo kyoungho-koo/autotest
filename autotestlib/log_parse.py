75a03279a11ec4ffaa3efcb558d2510e20c2d58a
HYmodrich <lwj0337@naver.com>
2020-01-11 18:56:21 +0900

    update log_parser.py

--- autotestlib/log_parse.py
+++ autotestlib/log_parse.py
@@ -3,25 +3,34 @@ import numpy as np
 
 def analysis(lines):
     lat_commit = []
-    lat_shadow = []
-    lat_forget = []
+#    lat_shadow = []
+#    lat_forget = []
 
     for line in lines:
-        json_string = line[line.find(']'+1:]
-        json_data = json.load(json_string)
+        #print (line)
+        try:
+            json_string = line[line.find('{'):line.find('}')+1]
+            json_data = json.loads(json_string)
 
-        lat_commit.append(json_data["lat_commit"])
-        lat_shadow.append(json_data["lat_shadow"])
-        lat_forget.append(json_data["lat_forget"])
+            lat_commit.append(json_data["lat_commit"])
+#           lat_shadow.append(json_data["lat_shadow"])
+#           lat_forget.append(json_data["lat_forget"])
 
-    print ( "avg commit lat : " + np.mean(lat_commit) + "\n"
-            "avg shadow lat : " + np.mean(lat_shadow) + "\n"
-            "avg forget lat : " + np.mean(lat_forget) + "\n")
+#            "avg shadow lat : " + np.mean(lat_shadow) + "\n"
+#            "avg forget lat : " + np.mean(lat_forget) + "\n")
+        except:
+            a = 1
+    print ( "avg commit lat : " + str(np.mean(lat_commit) )+ "\n")
+      #      print("") 
 
 
-Lines = []
-with open('~/filebench/atc20/log/') as log
-    Lines = log.readlines()
+for thread in [10 ,20 ,30 ,40 ,50]:
+    Lines = []
+    filename = 'data/sysbench/ext4/filesize_old/64G/log/nvme0n1thread'+str(thread) + '.log'
+    print ("======== nvme0n1thread" + str(thread) + ".log ===========")
 
-analysis(Lines)
+    with open(filename) as log:
+        Lines = log.readlines()
+
+    analysis(Lines)
 
